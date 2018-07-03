49492ffaa0e5bc64e6886aa5621ea8d8e6f78109
Phillip Jubb <phillip.jubb@coraluma.com>
2018-07-03 09:12:47 -0700

    scroll wheel

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
 /* fun little example using GreenSock's Draggable: https://www.greensock.com/draggable/ */
 
-var content = document.getElementById("content");
+var content = document.getElementById("dialcasing");
 var knob = document.getElementById("knob");
 var maxScroll = content.scrollHeight - content.offsetHeight;
 var sections = 9;
 
+var increment=51.428571428571429;
+
+function getColour(rotation){
+rotation=rotation % 360;  
+if(rotation<0) rotation=rotation+360;
+//if(rotation>360) rotation=rotation-360;
+  switch (true) {
+    case (rotation < increment):
+    console.log(rotation + " less than increment");
+        break;
+    case (rotation < increment*2):
+    console.log(rotation + " l<2 i");
+        break;
+    case (rotation < increment*3):
+    content.style.backgroundColor="blue";
+    console.log(rotation + " l<3 i");
+        break;
+    case (rotation < increment*4):
+    content.style.backgroundColor="yellow";
+    console.log(rotation + " l<4 i");
+        break;
+    case (rotation < increment*5):
+    content.style.backgroundColor="green";
+    console.log(rotation + " l<5 i");
+        break;
+    case (rotation < increment*6):
+    console.log(rotation + " l<6 i");
+        break;
+    case (rotation < increment*7):
+    console.log(rotation + " l<7 i");
+        break;
+    default:
+        break;
+}
+}
 
 
 var dragKnob = Draggable.create(knob, {
   type:"rotation",
-  bounds:{minRotation:0, maxRotation:360},
+  throwProps:true,
+  onClick:function() {
+		alert("clicked");
+	},
   onDrag: function() {
-    var scrollPos = map(this.x, 0, 360, 0, maxScroll)
-    console.log(scrollPos);
-    content.scrollTop = scrollPos;
+ //   var scrollPos = map(this.x, 0, 360, 0, maxScroll)
+ //   console.log(maxScroll + " and " + scrollPos);
+ //   content.scrollTop = scrollPos;
+    getColour(this.x);
+
   }
 })[0];
 
-content.addEventListener("scroll", requestUpdate);
-update();
-
+//content.addEventListener("scroll", requestUpdate);
+//update();
+/*
 function update() {
   var rotation = map(content.scrollTop, 0, maxScroll, 0, 360);
   console.log(rotation);
   TweenLite.set(knob, {rotation:rotation})
+  getColour(rotation);
   requestId = null;
 }
 
@@ -47,5 +88,5 @@ function norm(value, min, max) {
 	function map(value, sourceMin, sourceMax, destMin, destMax) {
 		return lerp(norm(value, sourceMin, sourceMax), destMin, destMax);
 	}
-
+*/
 
